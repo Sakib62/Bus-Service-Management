3261d661328b66fce046aa0c94b226e674534958
Safayet Rafi <safayetrafi461@gmail.com>
2023-10-11 10:30:25 +0600

    Admin dashboard feature UI created

--- bus-service-management/src/Components/Trips/Trips.js
+++ bus-service-management/src/Components/Trips/Trips.js
@@ -1,7 +1,30 @@
 import React from "react";
+import Profile from "../Icons/profile.png";
+import './Trips.css';
 
 export default function Trips(){
     return(
-        <h1>This is Trips</h1>
+     <div className="Trips-whole">
+        <div className="admin-home">
+        <div className="admin-header">
+            <h1>GreenLine</h1>
+            <img src={Profile}/>
+            <p>@admin</p>
+        </div>
+
+        <div className="admin-dashboard">
+            <h1>Dashboard</h1>
+            <button className="AdminButton">Bus</button>
+            <button className="TripsButton">Trips</button>
+            <button className="AdminButton">Booking</button>
+            <button className="AdminButton">Passenger</button>
+            <button className="AdminButton">Employee</button>
+        </div>
+        </div> 
+
+        <div className="Trips-information">
+            <h1>THis is Trips</h1>
+        </div>
+      </div>
     )
 }
