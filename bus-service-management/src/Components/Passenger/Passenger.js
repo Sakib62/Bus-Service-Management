7c1bb56e81672b500b4f48195a3422100c6d3b02
Safayet Rafi <safayetrafi461@gmail.com>
2023-10-11 10:30:25 +0600

    Admin dashboard feature UI created

--- bus-service-management/src/Components/Passenger/Passenger.js
+++ bus-service-management/src/Components/Passenger/Passenger.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Profile from "../Icons/profile.png";
-import './Booking.css';
+import './Passenger.css';
 
 export default function Booking(){
     return(
-     <div className="booking-whole">
+     <div className="Passenger-whole">
         <div className="admin-home">
             <div className="admin-header">
                 <h1>GreenLine</h1>
@@ -16,15 +16,16 @@ export default function Booking(){
                 <h1>Dashboard</h1>
                 <button className="AdminButton">Bus</button>
                 <button className="AdminButton">Trips</button>
-                <button className="BookingButton">Booking</button>
-                <button className="AdminButton">Passenger</button>
+                <button className="AdminButton">Booking</button>
+                <button className="PassengerButton">Passenger</button>
                 <button className="AdminButton">Employee</button>
             </div>
         </div> 
 
-        <div className="booking-information">
-            <h1>This is booking</h1>
+        <div className="passenger-information">
+            <h1>THis is Passenger</h1>
         </div>
+
       </div>
     )
 }
