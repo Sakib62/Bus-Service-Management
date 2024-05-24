1a404f936a43f4530b01d5125f25e268e2a60864
Sakib62 <sakib62.coder@gmail.com>
2024-05-25 00:09:29 +0600

    Added Dashboard Page and it's child pages

--- Client/src/pages/DashboardPage/Dashboard.jsx
+++ Client/src/pages/DashboardPage/Dashboard.jsx
@@ -4,16 +4,17 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import axios from "axios";
 
 const Dashboard = () => {
-  const anvigate = useNavigate()
+  const navigate = useNavigate()
   axios.defaults.withCredentials = true
   const handleLogout = () => {
-    axios.get('http://localhost:3000/auth/logout')
-    .then(result => {
-      if(result.data.Status) { 
-        localStorage.removeItem("valid")
-        anvigate('/')
-      }
-    })
+    navigate('/')
+    // axios.get('http://localhost:3000/auth/logout')
+    // .then(result => {
+    //   if(result.data.Status) { 
+    //     localStorage.removeItem("valid")
+    //     navigate('/')
+    //   }
+    // })
   }
   return (
     <div className="container-fluid">
@@ -21,77 +22,61 @@ const Dashboard = () => {
         <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
           <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
             
-            <ul
-              className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start"
-              id="menu"
-            >
+            <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+
               <li className="w-100">
-                <Link
-                  to="/dashboard"
-                  className="nav-link text-white px-0 align-middle"
-                >
+                <Link to="/dashboard" className="nav-link text-white px-0 align-middle">
                   <i className="fs-4 bi-speedometer2 ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Dashboard</span>
                 </Link>
               </li>
+
               <li className="w-100">
-                <Link
-                  to="/dashboard/employee"
-                  className="nav-link px-0 align-middle text-white"
-                >
+                <Link to="/dashboard/employee" className="nav-link px-0 align-middle text-white">
                   <i className="fs-4 bi-people ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">
-                    Manage Employees
-                  </span>
+                  <span className="ms-2 d-none d-sm-inline">Employee</span>
                 </Link>
               </li>
+
               <li className="w-100">
-                <Link
-                  to="/dashboard/category"
-                  className="nav-link px-0 align-middle text-white"
-                >
+                <Link to="/dashboard/category" className="nav-link px-0 align-middle text-white">
                   <i className="fs-4 bi-columns ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Category</span>
                 </Link>
               </li>
+
               <li className="w-100">
-                <Link
-                  to="/dashboard/passenger"
-                  className="nav-link px-0 align-middle text-white"
-                >
+                <Link to="/dashboard/passenger" className="nav-link px-0 align-middle text-white">
                   <i className="fs-4 bi-person ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Passenger</span>
                 </Link>
               </li>
+
               <li className="w-100">
-                <Link
-                  to="/dashboard/bus"
-                  className="nav-link px-0 align-middle text-white"
-                >
+                <Link to="/dashboard/bus" className="nav-link px-0 align-middle text-white">
                   <i className="fs-4 bi-bus-front ms-2"></i>
-                  <span className="ms-2 d-none d-sm-inline">Manage Bus</span>
+                  <span className="ms-2 d-none d-sm-inline">Bus</span>
                 </Link>
               </li>
+
               <li className="w-100">
-                <Link
-                  to="/dashboard/services"
-                  className="nav-link px-0 align-middle text-white"
-                >
+                <Link to="/dashboard/services" className="nav-link px-0 align-middle text-white">
                   <i className="fs-4 bi bi-tools ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Services</span>
                 </Link>
               </li>
+
               <li className="w-100" onClick={handleLogout}>
-              <Link
-                  className="nav-link px-0 align-middle text-white"
-                >
+                <Link className="nav-link px-0 align-middle text-white">
                   <i className="fs-4 bi-power ms-2"></i>
                   <span className="ms-2 d-none d-sm-inline">Logout</span>
                 </Link>
               </li>
+
             </ul>
           </div>
         </div>
+
         <div className="col p-0 m-0">
             <div className="p-2 d-flex justify-content-center shadow">
                 <h4>Bus Management System</h4>
